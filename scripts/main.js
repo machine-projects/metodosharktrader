b336e8c40324612eea7ff4ff328d0c6892f6c1ef
arthurnassar <arthurnassar@gmail.com>
2022-03-19 12:28:26 -0400

    lazy load adicionado as primeiras fotos e iframes

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,3 @@
-window.addEventListener("scroll", onScroll);
-
-function onScroll() {
-  for (var item of document.querySelectorAll(".animation")) {
-    elementVisible(item);
-  }
-}
-
-function elementVisible(el) {
-  let top = el.offsetTop;
-  let height = el.offsetHeight;
-  let bottom = top + height;
-
-  let IsOverBottom = top > (window.pageYOffset + window.innerHeight);
-  let IsBeforeTop = bottom < window.pageYOffset;
-
-  if (!IsOverBottom && !IsBeforeTop) {
-    el.classList.add("animate");
-  }
-}
-
 function showOrHideElement(element) {
   let getElement = document.querySelector(`${element}`).classList;
   let insHide = false;
