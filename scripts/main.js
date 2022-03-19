bce7ee81f7d34d70fb3efa87e20c6a149d1fb5e9
Felipe Toffoli <felipe_toffoli1@hotmail.com>
2022-03-19 09:56:10 -0400

    collapse body  e removendo formatação

--- scripts/main.js
+++ scripts/main.js
@@ -37,49 +37,53 @@ function showOrHideElement(element) {
 
 }
 
+function showBody() {
+  showOrHideElement('.body')
+  showOrHideElement('.button-header')
+}
 
+function timeShowBody() {
+  setTimeout(function () {
+    showBody()
+  }, 880000);
+}
 
+function startWebSite() {
+  timeShowBody()
+  componetTimer()
+}
 
+function componetTimer() {
 
+  // TIMER 
+  var minutes = 14;
+  var seconds = 60;
+  const minutesTimer = document.querySelector('#min-counter');
+  const secondsTimer = document.querySelector('#seconds-counter');
+  // contador dos minutos
+  setInterval(() => {
+    if (minutes == 0) {
+      clearInterval();
+    }
+    else {
+      minutes--;
+      minutesTimer.innerHTML = "<b>" + minutes + "</b>";
+    }
+  }, 60000)
+  // contador dos minutos
+  setInterval(() => {
+    if (minutes == 0) {
+      clearInterval();
+    }
+    else if (seconds == 0) {
+      seconds = 59
+    }
+    else {
+      seconds--;
+      secondsTimer.innerHTML = "<b>" + seconds + "</b>";
+    }
+  }, 1000)
 
+}
 
-
-
-
-
-
-
-// TIMER 
-var minutes = 14;
-var seconds = 60;
-const minutesTimer = document.querySelector('#min-counter');
-const secondsTimer = document.querySelector('#seconds-counter');
-
-// contador dos minutos
-setInterval(() => {
-  if (minutes == 0) {
-   clearInterval();
-  }
-  else {
-    minutes--;
-    minutesTimer.innerHTML = "<b>" + minutes + "</b>";
-  }
-}, 60000)
-
-// contador dos minutos
-setInterval(() => {
-  if (minutes == 0) {
-    clearInterval();
-  }
-  else if (seconds == 0) {
-    seconds = 59
-  }
-  else {
-    seconds--;
-    secondsTimer.innerHTML = "<b>" + seconds + "</b>";
-  }
-}, 1000)
-
-setTimeout(function () {
-  showOrHideElement('.body');
-}, 880000);
+startWebSite()
